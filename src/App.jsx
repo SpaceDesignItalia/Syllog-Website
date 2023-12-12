dd32ab97f7127ea4817e9797e0bb09fe6a442d3c
Gregorio Picchio <gregoriopicchio@gmail.com>
2023-12-12 13:41:18 +0100

    Navbar added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Home from "./Pages/Home/Home";
+import Topbar from "./Components/Global/Topbar";
 
 function App() {
   return (
+    <>
+    <Topbar/>
     <Routes>
       <Route exact path="/" element={<Home />} />
     </Routes>
+    </>
   );
 }
 
