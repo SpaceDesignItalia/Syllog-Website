6564ce820f0f6008d6f717d4849c1f547bf20070
Andrea Braia <andrix.braia@gmail.com>
2023-12-15 21:18:51 +0100

    Alcune animazioni

--- src/Pages/Home/Content.jsx
+++ src/Pages/Home/Content.jsx
@@ -1,16 +1,45 @@
 import React from "react";
 import { Button } from "@nextui-org/react";
+import { motion } from "framer-motion";
+import { useInView } from "react-intersection-observer";
 
 export default function Content() {
+  const [ref, inView] = useInView({
+    triggerOnce: true,
+    rootMargin: "-50% 0px",
+  });
+
+  console.log("InView:", inView);
+
   return (
     <>
-      <section>
+      <section ref={ref}>
         <div className="gap-16 items-center py-8 px-4 md:mx-auto md:max-w-screen-xl flex flex-col md:flex-row lg:justify-between lg:py-16 lg:px-6">
           <div className="md:w-1/2">
-            <h2 className="mb-4 text-4xl font-bold text-gray-900 dark:text-white">
+            <motion.h2
+              className="mb-4 text-4xl font-bold text-gray-900 dark:text-white"
+              initial={{ y: 150, opacity: 0 }}
+              animate={{ y: 0, opacity: 1 }}
+              transition={{
+                duration: 1.5,
+                type: "spring",
+                bounce: 0.25,
+                delay: 0.25,
+              }}
+            >
               Lorem ipsum dolor sit amet
-            </h2>
-            <p className="md:text-lg mb-4">
+            </motion.h2>
+            <motion.p
+              className="md:text-lg mb-4"
+              initial={{ y: 150, opacity: 0 }}
+              animate={{ y: 0, opacity: 1 }}
+              transition={{
+                duration: 1.5,
+                type: "spring",
+                bounce: 0.25,
+                delay: 0.5,
+              }}
+            >
               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
               eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
               enim ad minim veniam, quis nostrud exercitation ullamco laboris
@@ -19,11 +48,19 @@ export default function Content() {
               incididunt ut labore et dolore magna aliqua. Ut enim ad minim
               veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
               ex ea commodo consequat.
-            </p>
+            </motion.p>
           </div>
           <div className="mt-8 md:w-1/2">
-            <img
+            <motion.img
               className="mt-4 w-full lg:mt-10 rounded-lg"
+              initial={{ y: 150, opacity: 0 }}
+              animate={{ y: 0, opacity: 1 }}
+              transition={{
+                duration: 1.5,
+                type: "spring",
+                bounce: 0.25,
+                delay: 0.75,
+              }}
               src="https://i0.wp.com/bungalooknursery.com.au/wp-content/uploads/2022/12/test-image-Not-seen-on-Bungalook-Web-Page-used-for-testing-Image-related-stuff.jpg?w=1920&ssl=1"
               alt="office content 2"
             />
--- src/Pages/Home/Hero.jsx
+++ src/Pages/Home/Hero.jsx
@@ -1,37 +1,84 @@
 import React from "react";
 import { Button } from "@nextui-org/react";
 import HeroImg from "../../assets/Home/Hero.png";
+import { motion } from "framer-motion";
 
 export default function Hero() {
   return (
-    <section className="flex flex-col gap-20 justify-center items-center max-w-screen-2xl px-4 py-8 mx-auto lg:py-16 h-screen">
+    <motion.section className="flex flex-col gap-20 justify-center items-center max-w-screen-2xl px-4 py-8 mx-auto lg:py-16 h-screen">
       <div>
-        <h1 className="max-w-2xl mb-4 text-4xl font-bold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white text-center">
+        <motion.h1
+          className="max-w-2xl mb-4 text-4xl font-bold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white text-center"
+          initial={{ y: 150, opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
+          transition={{ duration: 1.5, type: "spring", bounce: 0.25 }}
+        >
           Gen AI Tools for the Learners of Tomorrow
-        </h1>
+        </motion.h1>
       </div>
       <div className="flex flex-col-reverse md:flex-row gap-5">
         <div className="flex flex-col justify-center w-full md:w-1/2">
-          <h1 className="text-center md:text-left max-w-2xl mb-4 text-4xl tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">
+          <motion.h1
+            className="text-center md:text-left max-w-2xl mb-4 text-4xl tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white"
+            initial={{ y: 150, opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+            transition={{
+              duration: 1.5,
+              type: "spring",
+              bounce: 0.25,
+              delay: 0.5,
+            }}
+          >
             Lorem ipsum dolor sit amet.
-          </h1>
-          <p className="text-center md:text-left max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">
+          </motion.h1>
+          <motion.p
+            className="text-center md:text-left max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400"
+            initial={{ y: 150, opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+            transition={{
+              duration: 1.5,
+              type: "spring",
+              bounce: 0.25,
+              delay: 0.75,
+            }}
+          >
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
             eiusmod tempor incididunt ut labore et dolore magna aliqua.
-          </p>
-          <Button
-            color="primary"
-            size="lg"
-            radius="sm"
-            className="w-full md:w-1/5"
+          </motion.p>
+          <motion.div
+            initial={{ y: 150, opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+            transition={{
+              duration: 1.5,
+              type: "spring",
+              bounce: 0.25,
+              delay: 1,
+            }}
           >
-            Sign up
-          </Button>
+            <Button
+              color="primary"
+              size="lg"
+              radius="sm"
+              className="w-full md:w-1/5"
+            >
+              Sign up
+            </Button>
+          </motion.div>
         </div>
-        <div className="w-full md:w-1/2">
-          <img src={HeroImg} alt="Hero" className=" object-contain" />
-        </div>
+        <motion.div
+          className="w-full md:w-1/2"
+          initial={{ y: 150, opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
+          transition={{
+            duration: 1.5,
+            type: "spring",
+            bounce: 0.45,
+            delay: 0.5,
+          }}
+        >
+          <img src={HeroImg} alt="Hero" className="object-contain" />
+        </motion.div>
       </div>
-    </section>
+    </motion.section>
   );
 }
