d1239dc606b09fb525f67ec1979b049044542796
Matteo Severino <matteoseve2@gmail.com>
2023-12-19 14:36:08 +0100

    Partnership

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,6 +1,7 @@
 //Utils
 import React from "react";
 import Hero from "./Hero";
+import Partnership from "./Partnership";
 import Content from "./Content";
 import FAQ from "./FAQ";
 
@@ -8,6 +9,7 @@ export default function Home() {
   return (
     <div>
       <Hero />
+      <Partnership />
       <Content />
       <FAQ />
     </div>
